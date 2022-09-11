8bc6d80ee79fafec6d5a8025d0da3cf4abc9d989
DarkLordRafa <93549989+DarkLordRafa@users.noreply.github.com>
2022-09-11 20:59:46 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./style.css";
 
 export default function App() {
   return (
+
     <div className="ball">
-      
+      <h1>Reac</h1>
+      <img src="" alt="Ícone do React">
     </div>
   );
 }
