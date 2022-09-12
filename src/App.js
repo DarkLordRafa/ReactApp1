dc41cceacb476e9cb4c6ebff4860b37ba9f6ab88
DarkLordRafa <93549989+DarkLordRafa@users.noreply.github.com>
2022-09-11 21:37:51 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./style.css";
+import reactIcon from "./react_icon.png";
 
 export default function App() {
   return (
 
-    <div className="ball">
+    <div className="container">
       <h1>React</h1>
-      <img src="images/react_icon.png" alt="Ícone do React"></img>
+      <img src={reactIcon} alt="Ícone do React"/>
     </div>
   );
 }
