e2b380814296a57b260f79d774f2d70c578d8c13
DarkLordRafa <93549989+DarkLordRafa@users.noreply.github.com>
2022-09-19 02:32:12 -0300

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import ReactInfo from "./components/react_info.js";
 import "./style.css";
+import "./css/react_info.css";
 
 export default function App() {
   return (
@@ -7,6 +9,7 @@ export default function App() {
     <div className="container">
       <h1>React</h1>
       <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="Ícone do React"/>
+      <ReactInfo/>
     </div>
-  );
+  )
 }
---
+++ src/components/react_info.js
@@ -0,0 +1,12 @@
+import React from "react";
+function ReactInfo(){
+  return(
+    <section className="ReactInfo">
+      <p>
+        O React é uma biblioteca JavaScript desenvolvida pela equipe do Facebook e que tem o conceito de reatividade, executando HTML dentro do JavaScript.
+      </p>
+    </section>
+  )
+}
+
+export default ReactInfo
