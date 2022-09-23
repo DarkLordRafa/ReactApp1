4a8271ce9a5c15fc03b5013a2bc61e9edfa2c247
DarkLordRafa <93549989+DarkLordRafa@users.noreply.github.com>
2022-09-23 00:35:49 -0300

    1 file added

---
+++ src/components/dev.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+function dev({name, age, area, objective}){
+
+  return(
+    <section>
+      <h2>Desenvolvedor</h2>
+      <ul>
+        <li>Nome: <span>{name}</span></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+  )
+}
+
+export default dev
