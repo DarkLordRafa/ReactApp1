d6d5ce9e5d86b4400dc91d7d3044685c9db1be83
DarkLordRafa <93549989+DarkLordRafa@users.noreply.github.com>
2022-09-26 19:24:09 -0300

    1 file modified

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -5,14 +5,12 @@ import styles from "../css/wheel.module.css";
 
 function Wheel(){
 
-  var wheelProps = {
-    react: "React",
-    props: "Props",
-    hooks: "Hooks",
-    jsx: "Jsx"
+  var wheelProp = {
+    name: ["Props", "Hooks", "Jsx", "Components"]
   };
   
   function randomProps(){
+    wheelProp.name = wheelProp.name[]
     return Math.floor((Math.random() * 5) - 1);
   }
   
@@ -21,7 +19,7 @@ function Wheel(){
     
     <section className ={styles.wheel_section}>
       <div className ={styles.wheel}>
-        <span className ={styles.wheel_text}>{wheelProps.[1]}</span>
+        <span className ={styles.wheel_text}>{wheelProps[1]}</span>
       </div>
       <div className ={styles.wheel_button}>
         <span>GIRAR</span>
